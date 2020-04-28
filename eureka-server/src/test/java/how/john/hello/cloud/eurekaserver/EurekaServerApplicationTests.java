e37f2920853c1bf97f7cae5b0bd4312ba8e4abbc
John Day <fall4her@gmail.com>
2020-04-28 18:25:18 +0800

    added Eureka Server

---
+++ eureka-server/src/test/java/how/john/hello/cloud/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package how.john.hello.cloud.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
