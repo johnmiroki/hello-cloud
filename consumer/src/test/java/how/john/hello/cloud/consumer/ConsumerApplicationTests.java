08855765168d0c217b3907f3314f81e4fcc12a56
John Day <fall4her@gmail.com>
2020-04-28 18:26:16 +0800

    added a consumer app, as eureka client, config server client and other services' consumer

---
+++ consumer/src/test/java/how/john/hello/cloud/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package how.john.hello.cloud.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
