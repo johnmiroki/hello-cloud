bda2012d2a3b96cd8294095cd9a6d105d471482f
John Day <fall4her@gmail.com>
2020-05-21 10:22:40 +0800

    added provider files

---
+++ service-provider/src/test/java/how/john/hello/cloud/serviceprovider/ServiceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package how.john.hello.cloud.serviceprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
